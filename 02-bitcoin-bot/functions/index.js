90eef75c4e922011a0e274e69c085fbfa54fd8f0
Alvaro Viebrantz <alvarowolfx@gmail.com>
2017-12-03 19:22:29 -0300

    [feat] Add welcome chips to the CryptoCurrency bot

--- 02-bitcoin-bot/functions/index.js
+++ 02-bitcoin-bot/functions/index.js
@@ -2,34 +2,71 @@
 
 const functions = require('firebase-functions');
 const { DialogflowApp } = require('actions-on-google');
+const fetch = require('node-fetch');
 
 const ACTION_PRICE = 'price';
-const ACTION_TOTAL = 'total';
+const ACTION_WELCOME = 'input.welcome';
 const ARG_CRYPTO_CURRENCY = 'CryptoCurrency';
 const ARG_CURRENCY = 'Currency';
+
+const BASE_API_URL = 'https://api.cryptonator.com/api';
+const TICKER_METHOD = '/ticker';
+
+const CRYPTO_CURRENCY_NAMES = {
+  doge: 'dogecoin',
+  btc: 'bitcoin',
+  eth: 'ethereum'
+};
+
+const SUGGESTIONS = [
+  'How much bitcoin costs',
+  'dogecoin in brl',
+  'ethereum in eur'
+];
 
 /**
  * @param {DialogflowApp} assistant
  */
 function priceHandler(assistant) {
-  let cryptoCurrency = assistant.getArgument(ARG_CRYPTO_CURRENCY);
-  let currency = assistant.getArgument(ARG_CURRENCY) || 'USD';
-  const msg = `Getting current price for ${cryptoCurrency} in ${
-    currency
-  } via Firebase`;
-  assistant.tell(msg);
+  const cryptoCurrency = assistant.getArgument(ARG_CRYPTO_CURRENCY);
+  const currency = assistant.getArgument(ARG_CURRENCY) || 'USD';
+  const formattedCryptoCurrency =
+    CRYPTO_CURRENCY_NAMES[cryptoCurrency] || cryptoCurrency;
+  const formattedCurrency = currency.toUpperCase();
+
+  const url = BASE_API_URL + TICKER_METHOD + `/${cryptoCurrency}-${currency}`;
+  fetch(url)
+    .then(res => res.json())
+    .then(res => {
+      let { ticker } = res;
+      let { price } = ticker;
+
+      const formattedPrice = parseFloat(price).toFixed(2);
+      const msg = `Right now the price of a ${formattedCryptoCurrency} is ${
+        formattedPrice
+      } ${formattedCurrency}.`;
+
+      assistant.ask(msg);
+    })
+    .catch(res => {
+      const msg = `Sorry, I cannot get the current price for ${
+        formattedCryptoCurrency
+      } right now. Try again later.`;
+      assistant.tell(msg);
+    });
 }
 
 /**
  * @param {DialogflowApp} assistant
  */
-function totalHandler(assistant) {
-  let cryptoCurrency = assistant.getArgument(ARG_CRYPTO_CURRENCY);
-  let currency = assistant.getArgument(ARG_CURRENCY) || 'USD';
-  const msg = `Getting total price for ${cryptoCurrency} in ${
-    currency
-  } via Firebase`;
-  assistant.tell(msg);
+function welcomeHandler(assistant) {
+  const msg = assistant
+    .buildRichResponse()
+    .addSimpleResponse(
+      'Welcome to CryptoCurrency Bot. Do you like to know the price for which cryptocurrency ?'
+    )
+    .addSuggestions(SUGGESTIONS);
+  assistant.ask(msg);
 }
 
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest(
@@ -38,7 +75,7 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest(
 
     const actionMap = new Map();
     actionMap.set(ACTION_PRICE, priceHandler);
-    actionMap.set(ACTION_TOTAL, totalHandler);
+    actionMap.set(ACTION_WELCOME, welcomeHandler);
 
     assistant.handleRequest(actionMap);
   }
