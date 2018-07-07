8e4366b6e866307052426bd1a16ca29adb528383
Gabriel Simplício <gabrielsdias@hotmail.com>
2018-07-06 23:50:55 -0400

    snippets adjustments

--- 02-cryptocoin-bot/snippets/02.js
+++ 02-cryptocoin-bot/snippets/02.js
@@ -2,7 +2,7 @@ const ARG_CRYPTO_CURRENCY = 'CryptoCurrency';
 const ARG_CURRENCY = 'Currency';
 
 const cryptoCurrency = assistant.parameters[ARG_CRYPTO_CURRENCY];
-const currency = assistant.parameters[ARG_CURRENCY] || 'USD';
+const currency = assistant.parameters[ARG_CURRENCY] || 'usd';
 
 const msg = `Getting current price for ${cryptoCurrency} in ${
   currency
